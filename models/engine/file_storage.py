102e6d9c2139f241080949cb7c7fc7051888ce06
elyessassi <mjoxen@gmail.com>
2024-03-27 12:04:06 +0000

    adding delete method and fixing all method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if (cls is None):
+            return FileStorage.__objects
+        else:
+            mydict = {}
+            for key ,value in __objects.items():
+                if value.__class__.__name__ == cls:
+                    mydict.update({key:value})
+            return mydict
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -34,6 +41,13 @@ class FileStorage:
         from models.city import City
         from models.amenity import Amenity
         from models.review import Review
+    
+    def delete(self, obj=None):
+        """Delete an object in the dictionary"""
+        try:
+            self.all().pop(obj.__class__.__name__,".",obj.id)
+        except IndexError():
+            pass
 
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
