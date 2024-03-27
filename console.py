b63d30e4c4fc4b340f352865a68564c534edef0a
elyessassi <mjoxen@gmail.com>
2024-03-27 11:37:08 +0000

    updating create command

--- console.py
+++ console.py
@@ -127,9 +127,13 @@ class HBNBCommand(cmd.Cmd):
         if len(args) > 1:
             mydict = storage.all()
             for i in (1, len(mylist) - 1):
+                print("----------------",mylist[i],"-------------")
                 key_and_value = mylist[i].split("=")
+                print("++++++++",key_and_value[0],"+++++++",key_and_value[1],"+++++++++++++++++++")
                 if type(key_and_value[1]) == str:
                     key_and_value[1] = key_and_value[1].replace("_", " ")
+                    key_and_value[1] = key_and_value[1].strip('"')
+                    print("++++++++",key_and_value[0],"+++++++",key_and_value[1],"+++++++++++++++++++")
                 mydict[f"{new_instance.__class__.__name__}.{new_instance.id}"].__dict__.update({key_and_value[0]:key_and_value[1]})
         print(new_instance.id)
         storage.save()
