0d7aa848fc24fde69618e0533eb9e6e503009d5e
Amenallah28 <amenallahmrabet9@gmail.com>
2024-03-29 14:09:41 +0100

    changed place

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,12 @@ Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), nullable = False, primary_key=True)
+    created_at = Column(DateTime , nullable= False,default=datetime.utcnow() )
+    updated_at = Column(DateTime,nullable = False, default = datetime.utcnow() )
+    
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        id = Column(String(60), nullable = False, primary_key=True)
-        created_at = Column(DateTime , nullable= False,default=datetime.utcnow() )
-        updated_at = Column(DateTime,nullable = False, default = datetime.utcnow() )
 
         if not kwargs:
             from models import storage
