7bc66946c5d63a384bf42cbed44b488e88e65f73
elyessassi <mjoxen@gmail.com>
2024-03-30 09:04:22 +0000

    just checking if the checker works

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,7 +11,7 @@ class FileStorage:
 
     def delete(self, obj=None):
         """delete obj from __objects if it’s inside
-        if obj is equal to None, the method should not do anything"""
+        if obj is equal to None, the method should not do any     thing"""
         if obj is not None:
             key = "{}.{}".format(obj.__class__.__name__, obj.id)
             if key in self.__objects.keys():
