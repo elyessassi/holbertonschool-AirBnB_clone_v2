af94d4df7d7222f50c6214879e683595194b8774
elyessassi <mjoxen@gmail.com>
2024-03-30 09:08:49 +0000

    fixing

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,17 +10,8 @@ class FileStorage:
     __objects = {}
 
     def delete(self, obj=None):
-        """delete obj from __objects if it’s inside
-        if obj is equal to None, the method should not do any     thing"""
-        if obj is not None:
-            key = "{}.{}".format(obj.__class__.__name__, obj.id)
-            if key in self.__objects.keys():
-                del (self.__objects[key])
-
-    def delete(self, obj=None):
         """Delete an object in the dictionary"""
         print("works")
-
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
