522ed73df24e8eec332b0af7abdd3ad7bf439d95
elyessassi <mjoxen@gmail.com>
2024-03-30 09:09:21 +0000

    just checking if the checker works

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,8 +10,14 @@ class FileStorage:
     __objects = {}
 
     def delete(self, obj=None):
-        """Delete an object in the dictionary"""
-        print("works")
+        """delete obj from __objects if it’s inside
+        if obj is equal to None, the method should not do any     thing"""
+        if obj is not None:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            if key in self.__objects.keys():
+                del (self.__objects[key])
+
+
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
