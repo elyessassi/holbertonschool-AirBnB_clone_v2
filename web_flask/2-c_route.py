b0d518c05e0063f0bcc98e40c90c8b396eb68abf
elyessassi <mjoxen@gmail.com>
2024-04-14 22:47:32 +0100

    adding c page with variable

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""create a test server that displays hello hbnb and hbnb"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+
+    """page displays hello hbnb"""
+
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """display HBNB"""
+
+    return "HBNB"
+
+
+@app.route("/c/<string:text>", strict_slashes=False)
+def c(text):
+    """display c with the text string"""
+
+    text = text.replace("_"," ")
+    return f"c {text}"
+
+if __name__ == "__main__":
+    app.run()
