8ca28e5f7d0f2261d134de7a60933021f980d9a4
elyessassi <mjoxen@gmail.com>
2024-07-22 06:28:17 +0100

    display if n is an int

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""create a test server that displays hello hbnb and hbnb"""
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """page displays hello hbnb"""
+
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """display HBNB"""
+
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """display c with the text string"""
+
+    text = text.replace("_", " ")
+    return f"C {escape(text)}"
+
+@app.route("/python/", defaults={"text": "is cool"})
+@app.route("/python/<text>")
+def python(text):
+    """display Python with text string with default value is cool"""
+
+    text = text.replace("_", " ")
+    return f"Python {escape(text)}"
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """display the number n if n is an int"""
+
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run()
